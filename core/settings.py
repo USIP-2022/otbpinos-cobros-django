53a48ee08ae03862e95fb7673e5088508acc6e48
mayleePerez <mayperez2010@gmail.com>
2022-09-23 23:22:36 -0400

    Iniciando el modulo de cobros

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cobros',  # <-- NEW
     'apps.home'  # Enable the inner home (home)
 ]
 
