305b7d7e9e83b2396b50d03da4908ec92e72ed13
mayleePerez <mayperez2010@gmail.com>
2022-10-07 23:55:07 -0400

    subiendo configuracion del admin pago, inmuebles, aportes y desembolsos

--- cobros/admin.py
+++ cobros/admin.py
@@ -5,6 +5,15 @@ from .models import Reunion
 from .models import Asistencia
 from .models import Gestion
 
+from .models import Multa
+from .models import PagoMulta
+from .models import Aporte
+from .models import InmuebleAporte
+from .models import PagoInmuebleAporte
+from .models import Pago
+from .models import Desembolsos
+from .models import Generico
+
 
 class inmuebleAdmin(admin.ModelAdmin):
     list_display = ("inmueble_nombre", "inmueble_numero", "propietario", "tipo_inmueble")
@@ -23,9 +32,36 @@ class reunionAdmin(admin.ModelAdmin):
 class asistenciaAdmin(admin.ModelAdmin):
     list_display = ("inmueble", "reunion", "tipo_asistencia")
 
+class multaAdmin(admin.ModelAdmin):
+    list_display = ("inmueble_id", "descripcion", "monto", "estado")
+
+class pagoMultaAdmin(admin.ModelAdmin):
+    list_display = ("multa_id", "monto_pagado", "estado")
+class aporteAdmin(admin.ModelAdmin):
+    list_display = ("monto", "descripcion", "fecha_inicio")
+class inmuebleAporteAdmin(admin.ModelAdmin):
+    list_display = ("inmueble_id", "estado")
+class pagoInmuebleAporteAdmin(admin.ModelAdmin):
+    list_display = ("inmueble_aporte_id", "monto_pagado", "estado")
+class pagoAdmin(admin.ModelAdmin):
+    list_display = ("inmueble_id", "monto", "reunion_id", "estado")
+class desembolsoAdmin(admin.ModelAdmin):
+    list_display = ("motivo", "monto", "numero_boleta", "estado")
+class genericoAdmin(admin.ModelAdmin):
+    list_display = ("propiedad", "descripcion", "valor", "data", "estado")
+
 
 admin.site.register(Propietario, propietarioAdmin)
 admin.site.register(Inmueble, inmuebleAdmin)
 admin.site.register(Gestion, gestionAdmin)
 admin.site.register(Reunion, reunionAdmin)
 admin.site.register(Asistencia,asistenciaAdmin)
+
+admin.site.register(Multa,multaAdmin)
+admin.site.register(PagoMulta,pagoMultaAdmin)
+admin.site.register(Aporte,aporteAdmin)
+admin.site.register(InmuebleAporte,inmuebleAporteAdmin)
+admin.site.register(PagoInmuebleAporte,pagoInmuebleAporteAdmin)
+admin.site.register(Pago,pagoAdmin)
+admin.site.register(Desembolsos,desembolsoAdmin)
+admin.site.register(Generico,genericoAdmin)
